472d59fe00627cf81d68b245d4fa05a70990f279
Corentin Cadiou <contact@cphyc.me>
2020-10-21 21:56:55 +0100

    Take my own comments into account

--- src.js
+++ src.js
@@ -1,8 +1,8 @@
 let outlook_regex = /https:\/\/\w+\.safelinks\.protection\.outlook\.com\/\?url=(\S+(?:\&amp;data))/g;
 let proofpoint_regex = new RegExp('https://urldefense(?:\.proofpoint)?\.com/(v[0-9])/');
 
-function safelinkDecoder (str) {
-    return str.replace(outlook_regex, (safelinkUri, encodedUri) => decodeURIComponent(encodedUri));
+function safelinkDecoder (a) {
+    return a.replace(outlook_regex, (safelinkUri, encodedUri) => decodeURIComponent(encodedUri));
 }
 
 function proofPointDecoder (a) {
@@ -17,27 +17,24 @@ function proofPointDecoder (a) {
         outurl = decodeURIComponent(a.match(v1_pattern)[1]);
     } else if (v == 'v2') {
         let v2_pattern = new RegExp('https://urldefense(?:\.proofpoint)?\.com/v2/url\\?u=([^&]*)&[dc]=.*');
-        var url = a.match(v2_pattern)[1];
-        url = url.replace(/-/g, '%');
-        url = url.replace(/_/g, '/');
+        let url = a.match(v2_pattern)[1].replace(/-/g, '%').replace(/_/g, '/');
         outurl = decodeURIComponent(url);
     } else if (v == 'v3') {
         let v3_pattern = new RegExp('https://urldefense(?:\.proofpoint)?\.com/v3/__(.+)__;([^\!]*).*');
         let v3_token_pattern = new RegExp('\\*(\\*.)?', 'g');
         let length_codes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
-        var url = a.match(v3_pattern);
-        var encbytes = atob(url[2].replace(/_/g, '/').replace(/-/g, '+'));
-        var encbytes_off = 0;
+        let url = a.match(v3_pattern);
+        let encbytes = atob(url[2].replace(/_/g, '/').replace(/-/g, '+'));
+        let encbytes_off = 0;
 
-        function insert_encbytes(chunk) {
+        outurl = url[1].replace(v3_token_pattern, (chunk) => {
             var len = 1;
             if (chunk.length > 1)
                len = length_codes.search(chunk[2]) + 2;
             var out = encbytes.substring(encbytes_off, encbytes_off + len);
             encbytes_off += len;
             return out;
-        }
-        outurl = url[1].replace(v3_token_pattern, insert_encbytes);
+        });
     }
 
     return outurl;
