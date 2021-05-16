451620dddef3c09447ac0bf0bee6fbb43e568afd
Jan Kiszka <jan.kiszka@web.de>
2021-05-16 18:05:06 +0200

    Add another decoder test pattern for safelinks
    This contains some domain name in the data field, checking that the
    decoder accepts '.' in the middle of the URL.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

--- test/tests.js
+++ test/tests.js
@@ -22,11 +22,13 @@ describe('Decoder', function() {
     let encoded_urls = [
       "https://eur01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fmochajs.org%2F%23getting-started&amp;data=04%7C01%7C%7C2f36b70a94f44000effb08d8760186f7%7C1faf88fea9984c5b93c9210a11d9a5c2%7C0%7C1%7C637389096241229831%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=oCPIK9For1kcpd6Vog9%2FD0k6LGLW%2F9aZPU4T1pZdukQ%3D&amp;reserved=0",
       "https://eur01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2F68%25E2%2580%259395%25E2%2580%259399.7_rule&amp;data=04%7C01%7C%7C2f36b70a94f44000effb08d8760186f7%7C1faf88fea9984c5b93c9210a11d9a5c2%7C0%7C1%7C637389096241229831%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&amp;sdata=sHyEH2d9fkO%2Fe89wxD4%2B9Zxxt7EPwbRiJD2lDaDr6Ts%3D&amp;reserved=0",
+      "https://eur01.safelinks.protection.outlook.com/?url=https%3A%2F%2Flangbein.org%2Fmicrosoft-outlook-office365-url-rewriting-in-e-mails%2F&amp;data=04%7C01%7C2222b23b-5f46-47a1-befe-434e1451c24cc%40abc.domain.org%7Cbcd2a86092b641ffbec408d915f7bc0c%7C38ae3bae5794f12addab42e1495d55a%7C1%7C0%7C637564976765719515%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0&amp;sdata=7CWDNfiFj%2FaOiu71%2Fe3IFJ9WtJb36c0nYeKXyIJWsvs%3D&amp;reserved=0",
       "https://na01.safelinks.protection.outlook.com/?url=http%3a%2f%2fwww.avanan.com"
     ];
     let expected_urls = [
       "https://mochajs.org/#getting-started",
       "https://en.wikipedia.org/wiki/68%E2%80%9395%E2%80%9399.7_rule",
+      "https://langbein.org/microsoft-outlook-office365-url-rewriting-in-e-mails/",
       "http://www.avanan.com"
     ];
 
